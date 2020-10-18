b4d77c78ba2b07295129fe340b1a27b6bb8c2bfa
NarutoBratishka <katorabian@gmail.com>
2020-10-18 21:50:36 +0700

    My early homework about restaurant working

---
+++ Restaurant/src/RestPackage/Table.java
@@ -0,0 +1,10 @@
+package RestPackage;
+
+public class Table {
+    private static byte tableNumber;
+    private byte number = ++tableNumber;
+
+    public Order getOrder() {
+        return new Order(number);
+    }
+}
